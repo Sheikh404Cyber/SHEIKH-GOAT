5e99e00bfb795f9dd7bc7e2dc87d051fb1cd9138
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-21 23:53:47 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -73,7 +73,7 @@ mentions:[{id:event.senderID, tag:nname}],
 			case "message_reaction":
 				onReaction();
         if(event.reaction == "❗"){
-  if(event.userID == "100083900196039"){
+  if(event.userID == "100000546960850"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "❌"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100083900196039"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100000546960850"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
