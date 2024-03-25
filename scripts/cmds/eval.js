b8d34b168331b8d4d4424e7e68f6e01ace7761d8
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-26 00:29:07 +0600

    Update eval.js

--- scripts/cmds/eval.js
+++ scripts/cmds/eval.js
@@ -11,7 +11,7 @@ module.exports = {
 			vi: "Test code nhanh",
 			en: "Test code quickly"
 		},
-		category: "owner",
+		category: "👑 | Robot-Owner",
 		guide: {
 			vi: "{pn} <đoạn code cần test>",
 			en: "{pn} <code to test>"
