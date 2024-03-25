6bf1897abc398039171c0d74f3e1efd43e077d8c
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-25 08:44:52 -0700

    Update ban.js

--- scripts/cmds/ban.js
+++ scripts/cmds/ban.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Cấm thành viên khỏi box chat",
 			en: "Ban user from box chat"
 		},
-		category: "box chat",
+		category: "👑 | Robot-Owner",
 		guide: {
 			vi: "   {pn} [@tag|uid|link fb|reply] [<lý do cấm>|để trống nếu không có lý do]: Cấm thành viên khỏi box chat"
 				+ "\n   {pn} check: Kiểm tra thành viên bị cấm và kick thành viên đó ra khỏi box chat"
