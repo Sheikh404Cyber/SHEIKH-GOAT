00b80228361aca587e1d04298179304c96af65c3
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-26 03:39:17 -0700

    Update translate.js

--- scripts/cmds/translate.js
+++ scripts/cmds/translate.js
@@ -13,7 +13,7 @@ module.exports = {
 			vi: "Dịch văn bản sang ngôn ngữ mong muốn",
 			en: "Translate text to the desired language"
 		},
-		category: "utility",
+		category: "👥 | Member",
 		guide: {
 			vi: "   {pn} <văn bản>: Dịch văn bản sang ngôn ngữ của box chat bạn hoặc ngôn ngữ mặc định của bot"
 				+ "\n   {pn} <văn bản> -> <ISO 639-1>: Dịch văn bản sang ngôn ngữ mong muốn"
