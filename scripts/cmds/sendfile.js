f6b145d9febb50c7440ba8f53b5e760ccab17a3a
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-26 02:39:54 -0700

    Update sendfile.js

--- scripts/cmds/sendfile.js
+++ scripts/cmds/sendfile.js
@@ -9,7 +9,7 @@ module.exports = {
     role: 2,
     shortDescription: "Send bot script",
     longDescription: "Send bot specified file ",
-    category: "owner",
+    category: "👑 | Robot-Owner",
     guide: "{pn} file name. Ex: .{pn} filename"
   },
 
