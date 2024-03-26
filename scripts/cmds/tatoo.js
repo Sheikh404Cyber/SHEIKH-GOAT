0092d8535c98933a6a4b1aec6cf7f3a701c30cbc
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-26 03:20:33 -0700

    Update tatoo.js

--- scripts/cmds/tatoo.js
+++ scripts/cmds/tatoo.js
@@ -10,7 +10,7 @@ module.exports = {
     role: 0,
     shortDescription: "Tatoo image!",
     longDescription: "",
-    category: "fun",
+    category: "👥 | Member",
     guide: "{pn} [mention|leave_blank]",
     envConfig: {
       deltaNext: 5
