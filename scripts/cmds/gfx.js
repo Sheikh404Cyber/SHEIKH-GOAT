57af84c640133e449b4f925c13d64fded958c0eb
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-25 23:07:11 -0700

    Update gfx.js

--- scripts/cmds/gfx.js
+++ scripts/cmds/gfx.js
@@ -4,15 +4,15 @@ const fs = require("fs");
 
 module.exports = {
 	config: {
-		name: "gfx",
-    aliases: ["gfxs"],
+		name: "logo1",
+    aliases: ["gfx"],
     version: "1.0",
 		author: "Samir",
 		countDown: 5,
 		role: 0,
 		shortDescription: "Make A gfx logo",
 		longDescription: "Make A gfx logo",
-		category: "gfx",
+		category: "🎨 | Logo Art",
 		guide: {
       en: "{p}{n} name",
     }
