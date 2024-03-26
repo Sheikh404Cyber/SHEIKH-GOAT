0719d563ddcc7313692a211ff2a1c5c7607db403
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-26 04:08:14 -0700

    Update ytb.js

--- scripts/cmds/ytb.js
+++ scripts/cmds/ytb.js
@@ -31,7 +31,7 @@ module.exports = {
 			vi: "Tải video, audio hoặc xem thông tin video trên YouTube",
 			en: "Download video, audio or view video information on YouTube"
 		},
-		category: "media",
+		category: "⏳ | Downloader",
 		guide: {
 			vi: "   {pn} [video|-v] [<tên video>|<link video>]: dùng để tải video từ youtube."
 				+ "\n   {pn} [audio|-a] [<tên video>|<link video>]: dùng để tải audio từ youtube"
