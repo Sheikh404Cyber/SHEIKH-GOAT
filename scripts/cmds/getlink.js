8c1eefafc023764fd1efb706b1922c107f6bb6b9
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-25 22:59:16 -0700

    Update getlink.js

--- scripts/cmds/getlink.js
+++ scripts/cmds/getlink.js
@@ -9,7 +9,7 @@ module.exports = {
     role: 0,
     shortDescription: "Get download url from video, audio sent from group",
     longDescription: "Get download url from video, audio sent from group",
-    category: "utility",
+    category: "👥 | Member",
     guide: "{pn} reply or add link of image"
   },
   
