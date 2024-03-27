e3f81d7039a0829da9ee43c390e0a44631a26ff7
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-03-27 13:29:16 +0600

    Update autodl.js

--- scripts/cmds/autodl.js
+++ scripts/cmds/autodl.js
@@ -1,65 +1,73 @@
 const axios = require('axios');
-const fs = require('fs');
-const { shortenURL } = global.utils;
-module.exports = {
-  config: {
-    name: "autodl", 
-    version: "1.0.1",
-    author: "Dipto",
-    countDown: 0,
-    role: 0,
-    description: {
-      en: "Auto download video from tiktok, facebook, Instagram, YouTube, and more"
-    },
-    category: "⏳ | Downloader",
-    guide: {
-      en: "[video_link]"
-    }
-},
-  onStart: async function(){},
-  onChat: async function ({ api, event }) {
+const fs = require('fs-extra');
+const tinyurl = require('tinyurl');
 
+module.exports.config = {
+  name: "autodl",
+  version: "1.0.",
+  role: 0,
+  author: "Dipto",
+  description: "Fb Vid Downloader",
+  category: "⏳ | Downloader",
+  guide: {en:"fb video link"},
+   usePrefix: true,
+  coolDowns: 2
+};
+
+module.exports.onChat = async function ({ api, event, client, __GLOBAL }) {
 let dipto = event.body ? event.body : '';
-
   try {
-    if (dipto.startsWith('https://vt.tiktok.com') || dipto.startsWith('https://www.tiktok.com/') || dipto.startsWith('https://www.facebook.com') || dipto.startsWith('https://www.instagram.com/') || dipto.startsWith('https://youtu.be/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://vm.tiktok.com') || dipto.startsWith('https://fb.watch')){
-
-      api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
-
-    const path = __dirname + `/cache/diptoo.mp4`;
-
-    const { data } = await axios.get(`https://noobs-apihouse.onrender.com/dipto/aldl?url=${encodeURIComponent(dipto)}`);
-
-    const vid = (await axios.get(data.result, { responseType: "arraybuffer", })).data;
-
+if (dipto.startsWith('https://vt.tiktok.com') ||
+dipto.startsWith("https://vm.tiktok.com") ||
+dipto.startsWith('https://www.facebook.com') || 
+dipto.startsWith('https://fb.watch')||
+dipto.startsWith('https://www.instagram.com/')|| dipto.startsWith('https://youtu.be/') ||
+dipto.startsWith('https://www.instagram.com/p/') || dipto.startsWith('https://pin.it/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://www.capcut.com/') || dipto.startsWith('https://www.threads.net/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://l.likee.video/')){
+  api.sendMessage("wait baby", event.threadID, event.messageID);
+  if (!dipto) {
+    api.sendMessage("please put a valid fb video link", event.threadID, event.messageID);
+    return;
+    }
+const aa = await axios.get(`${global.config.API}/dipto/alldl?url=${encodeURIComponent(dipto)}`);
+   const bb = aa.data;
+   const shortUrl = await tinyurl.shorten(bb.result);
+   const MSG = `✅ 🔗 Download Url: ${shortUrl}`;
+   let ex;
+   let cp;
+        if (bb.result.includes('.jpg')){
+             ex = ".jpg";
+             cp = "Here's your Photo <😘";
+        }
+        else if (bb.result.includes('.png')){
+             ex = ".png";
+             cp = "Here's your Photo <😘";
+        }
+        else if (bb.result.includes('.jpeg')){
+             ex = ".jpeg";
+             cp = "Here's your Photo <😘";
+        }
+        else { 
+          ex = ".mp4";
+          cp = bb.cp;
+        }
+const path = __dirname + `/cache/video${ex}`;
+    const vid = (await axios.get(bb.result, { responseType: "arraybuffer", })).data;
     fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
-       api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-
     api.sendMessage({
-
-        body: `𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐯𝐢𝐝𝐞𝐨 𝐜𝐨𝐦𝐩𝐢𝐥𝐚𝐭𝐞☺︎︎😘`,
-
-      attachment: fs.createReadStream(path)
-    }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
-
+      body: `${cp}\n${MSG}\n 🥹💙`,
+      attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
 if (dipto.startsWith('https://i.imgur.com')){
-
   const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
-
   const response = await axios.get(dipto, { responseType: 'arraybuffer' });
-
 const filename = __dirname + `/cache/dipto${dipto3}`;
-
     fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
-    api.sendMessage({body: `✅ | Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
-
+    api.sendMessage({body: `Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
   () => fs.unlinkSync(filename),event.messageID)
-
 }
-
 } catch (e) {
-    api.setMessageReaction("❎", event.messageID, (err) => {}, true);
-api.sendMessage(e, event.threadID, event.messageID);
-  }
-  },
+api.sendMessage(`${e}`, event.threadID, event.messageID);
+  };
+};
+module.exports.onStart = function({ api, event, client, __GLOBAL }) {
+
 }
