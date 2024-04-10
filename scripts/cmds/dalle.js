eb2ae7d8327e0e4cbffd7fa4062dc9162087ce9c
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-04-11 04:00:27 +0600

    Update dalle.js

--- scripts/cmds/dalle.js
+++ scripts/cmds/dalle.js
@@ -32,9 +32,12 @@ const { getStreamFromURL } = global.utils;
       const prompt = args.join(" ");
       const encodedPrompt = encodeURIComponent(prompt);
       const apiKey = "rubish69";
-      const cookies = ["1mKJ_7BktjdJvvIdg9vwc--roaaype5jCUiMns-gn7e6ES52UlCoYJ9hnolPm44wFO3wuL2nKQkx6Kq2Evq5mKFF1ewuIkgau-YqAlfV5W3bzM0BUbU2RDAbAia3z0goRzr1u1g55RamhgLc2qKYcMCv-_X5vmlufpInUT4jLRqccYmMpu20YXsoKcq99XA0wU0w9xLFNtwLv7QOMu5vGaH_NdnOfVtDEkf14WIdij6E", //ghost
+      const cookies = ["1CFjZEG3PLy9ByYhKMJWAGso5-3iYd7jj_xbObxFfnd6haHprXsI-Xcle0QnreR3VpNh9esqiuQxNa8SVp1GYYiV0mfH9IeI9fnWvBGbAtKvd1sYkcZ4n6BDEnnPbnyMEMPV1GNNLdFBsTcj3wDuyQgsVHX15Djr2iGyqszQwOTDS--atcwZKOGK15IXayA-5IOmA8mSCrlPVcMx1L4zXsA",
+                       "1oYqh4jobg6uWVy0gQoFofdBJvot1fDU3Du9eLaI95sY3x3btJYOvCLxQP_i4rpx7fwMe-VJaxsEdCmFLj8uISBb4oNGaOe6QdHg4C44nNtqnfOzgMwYXyKgNulScaokTj5fNu-37ZetATMi2zpJz-NjAD4Fy6BGVkhQ530FKjAi77C3ihMpcUTVYHCON9kH1IMT41Id5xvc3dTdQBmgSlw",
+                       "1TTzmloxcxAbj1pWCQ7-BDDf8zhlo_OkY3H1J2WSn8CSnbwir14uZOf4SlNBu1PGvUiiWvGSOJFaEi7NezZfbTs2C347fWM-2KJ6ie1yndkX8VC6m7NLnfjyzR2wB7g0p_1IyYymBGyKKmR4ocVjByq4l-AUGV_JyuB_9vE-OB90mkvEvzswrgH7Wbzr5Qc6a4i3Ez0ZBkhl9y_Bg_-72LQ",
+"1__pj3WwMGUAajvRLJsc63C69v9Fx3313Fv_8azhIelWgIKPoIMVY-AImphNPfPZd7C_gxGfR3AGUnbRibxKr8otAykBs4Gsxg47GsgZmgHk6tAm7mYg9PxEvIKkFTzXdaoH8wHFOt5fe3OISAmh92ru_uHnDE5sVg0UySpSVGztIg0Z-I7ZgnspnnP2gGuKX0LJjqm3wsN_wTpviMSVHOAONE8CD3iSs93dSoGbpaIU",
 
-"", //mahi
+
 
 ]; 
 
@@ -88,7 +91,7 @@ Processing Time: ${processingTimeInSeconds}s`,
       console.error(error);
 
       if (error.response && error.response.status === 401) {
-        await message.reply("⚠ | Unauthorized your API key \n\nContact with Rubish for a new apikey");
+        await message.reply("⚠ | Unauthorized your API key \n\nContact with Amin Sheikh for a new apikey");
       } else if (error.response && error.response.data) {
         const responseData = error.response.data;
 
@@ -100,7 +103,7 @@ Processing Time: ${processingTimeInSeconds}s`,
         } else if (error.response.status === 404) {
           await message.reply("⚠ | The DALL·E-3 API endpoint was not found. Please check the API URL.");
         } else {
-          await message.reply(`⚠ | Rubish DALL·E-3 server busy now\n\nPlease try again later`);
+          await message.reply(`⚠ | DALL·E-3 server busy now\n\nPlease try again later`);
         }
       } else {
         await message.reply("⚠ | An unexpected error occurred. Please try again later.");
