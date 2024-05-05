411fb359a6e214059d9ab9e914d2a6631af03415
Sheikh404Cyber <150942663+Sheikh404Cyber@users.noreply.github.com>
2024-05-06 01:15:25 +0600

    Update dalle.js

--- scripts/cmds/dalle.js
+++ scripts/cmds/dalle.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { getStreamFromURL } = global.utils;
+const rubishapi = global.GoatBot.config.rubishapi;
 
   module.exports = {
     config: {
@@ -8,7 +9,7 @@ const { getStreamFromURL } = global.utils;
       version: "2.0",
       author: "RUBISH",
       shortDescription: {
-        en: "An AI-based image generator using DALL·E and Bing technology"
+        en: "An AI-based image generator using DALL·E 3 technology"
       },
       longDescription: {
         en: "Bing is an AI module that leverages the latest DALL·E 3 technology to generate images based on given prompts. It provides users with creative and unique images tailored to their inputs."
@@ -25,25 +26,22 @@ const { getStreamFromURL } = global.utils;
   onStart: async function ({ message, args }) {
     try {
       if (args.length === 0) {
-        await message.reply("⚠ | Please provide a prompt\n\nExample ► .dalle-bing A beautiful Girl");
+        await message.reply("⚠️ | Please provide a prompt\n\nExample ► .dalle2 A beautiful Girl");
         return;
       }
 
       const prompt = args.join(" ");
       const encodedPrompt = encodeURIComponent(prompt);
       const apiKey = "rubish69";
-      const cookies = ["1CFjZEG3PLy9ByYhKMJWAGso5-3iYd7jj_xbObxFfnd6haHprXsI-Xcle0QnreR3VpNh9esqiuQxNa8SVp1GYYiV0mfH9IeI9fnWvBGbAtKvd1sYkcZ4n6BDEnnPbnyMEMPV1GNNLdFBsTcj3wDuyQgsVHX15Djr2iGyqszQwOTDS--atcwZKOGK15IXayA-5IOmA8mSCrlPVcMx1L4zXsA",
+      const cookies = [
+        "1CFjZEG3PLy9ByYhKMJWAGso5-3iYd7jj_xbObxFfnd6haHprXsI-Xcle0QnreR3VpNh9esqiuQxNa8SVp1GYYiV0mfH9IeI9fnWvBGbAtKvd1sYkcZ4n6BDEnnPbnyMEMPV1GNNLdFBsTcj3wDuyQgsVHX15Djr2iGyqszQwOTDS--atcwZKOGK15IXayA-5IOmA8mSCrlPVcMx1L4zXsA",
                        "1oYqh4jobg6uWVy0gQoFofdBJvot1fDU3Du9eLaI95sY3x3btJYOvCLxQP_i4rpx7fwMe-VJaxsEdCmFLj8uISBb4oNGaOe6QdHg4C44nNtqnfOzgMwYXyKgNulScaokTj5fNu-37ZetATMi2zpJz-NjAD4Fy6BGVkhQ530FKjAi77C3ihMpcUTVYHCON9kH1IMT41Id5xvc3dTdQBmgSlw",
                        "1TTzmloxcxAbj1pWCQ7-BDDf8zhlo_OkY3H1J2WSn8CSnbwir14uZOf4SlNBu1PGvUiiWvGSOJFaEi7NezZfbTs2C347fWM-2KJ6ie1yndkX8VC6m7NLnfjyzR2wB7g0p_1IyYymBGyKKmR4ocVjByq4l-AUGV_JyuB_9vE-OB90mkvEvzswrgH7Wbzr5Qc6a4i3Ez0ZBkhl9y_Bg_-72LQ",
-"1__pj3WwMGUAajvRLJsc63C69v9Fx3313Fv_8azhIelWgIKPoIMVY-AImphNPfPZd7C_gxGfR3AGUnbRibxKr8otAykBs4Gsxg47GsgZmgHk6tAm7mYg9PxEvIKkFTzXdaoH8wHFOt5fe3OISAmh92ru_uHnDE5sVg0UySpSVGztIg0Z-I7ZgnspnnP2gGuKX0LJjqm3wsN_wTpviMSVHOAONE8CD3iSs93dSoGbpaIU",
-
-
-
 ]; 
 
       const randomCookie = cookies[Math.floor(Math.random() * cookies.length)];
 
-      const apiURL = `https://dall-e-3-rubish-api.onrender.com/api/gen-img-url?prompt=${encodedPrompt}&cookie=${randomCookie}&apiKey=${apiKey}`;
+      const apiURL = `${rubishapi}/dalle?prompt=${encodedPrompt}&cookie=${randomCookie}&apikey=${apiKey}`;
 
       const startTime = Date.now();
       const processingMessage = await message.reply(`
@@ -61,7 +59,7 @@ Please wait a few seconds...`);
       const data = response.data;
       if (!data.imageLinks || data.imageLinks.length === 0) {
         if (data.errorMessage === "Invalid API key") {
-          await message.reply("⚠ | Invalid API key. Please check your API key and try again.");
+          await message.reply("⚠️ | Invalid API key. Please check your API key and try again.");
         } else {
           await message.reply(`
 ⭕ | No images found for the 
@@ -91,23 +89,16 @@ Processing Time: ${processingTimeInSeconds}s`,
       console.error(error);
 
       if (error.response && error.response.status === 401) {
-        await message.reply("⚠ | Unauthorized your API key \n\nContact with Amin Sheikh for a new apikey");
+        await message.reply("⚠️ | Unauthorized your API key \n\nContact with Rubish for a new apikey");
       } else if (error.response && error.response.data) {
         const responseData = error.response.data;
 
         if (responseData.errorMessage === "Pending") {
-          await message.reply("⚠ | This prompt has been blocked by Bing. Please try again with another prompt.");
+          await message.reply("⚠️ | This prompt has been blocked by Bing. Please try again with another prompt.");
         } else if (typeof responseData === 'object') {
           const errorMessages = Object.entries(responseData).map(([key, value]) => `${key}: ${value}`).join('\n');
-          await message.reply(`⚠ | Server error details:\n\n${errorMessages}`);
+          await message.reply(`⚠️ | Server error details:\n\n${errorMessages}`);
         } else if (error.response.status === 404) {
-          await message.reply("⚠ | The DALL·E-3 API endpoint was not found. Please check the API URL.");
+          await message.reply("⚠️ | The DALL·E-3 API endpoint was not found. Please check the API URL.");
         } else {
-          await message.reply(`⚠ | DALL·E-3 server busy now\n\nPlease try again later`);
-        }
-      } else {
-        await message.reply("⚠ | An unexpected error occurred. Please try again later.");
-      }
-    }
-  }
-};
+          await message.reply(`⚠️ | Rubish DALL·E-3 server busy now\n\nPlease try again later`); 
